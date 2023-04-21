9dd3c9041b127ca6edc722835ac9435a38c6aeff
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-04-21 10:37:59 +0600

    card add to caulator

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,18 +6,23 @@ const Cart = ({ cart }) => {
     // const {cart} = props;   option 2
 
     let total = 0;
+    let totalShipping = 0;
     for (const product of cart) {
         total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+    const tax = total * 7 / 100;
+
+    const grandTotal = total + totalShipping + tax;
 
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Select Item: {cart.length}</p>
             <p>Total price: ${total}</p>
-            <p>Shipping: </p>
-            <p>Tax: </p>
-            <h6>Grand Total: </h6>
+            <p>Shipping: {totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
